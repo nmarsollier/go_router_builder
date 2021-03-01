816b8e3c3b713d1737ec3496066a0a494ca50750
nmarsollier <nmarsollier@gmail.com>
2021-03-01 14:22:49 -0300

    Initial commit

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
-import (
-	"github.com/nmarsollier/go_router_builder/controllers"
-)
+import "github.com/nmarsollier/go_router_builder/rest/routes"
 
 func main() {
-	controllers.Start()
+	routes.Start()
 }
